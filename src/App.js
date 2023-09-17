0f4024e5932a9d9476e228817556560eb6bd43bb
hargun13 <chandhokhargun@gmail.com>
2023-09-17 09:46:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Sidebar from './components/Sidebar';
+import Login from './login_signup/Login';
+import Signup from './login_signup/Signup';
+import Community from './user/CommunitySection/Community';
+import Education from './user/Education/Education';
+import BlogsPage from './user/Education/Blog/BlogsPage';
+import VideosPage from './user/Education/Videos/VideosPage';
+import Insurance from './user/Insurance/Insurance';
+import GovScehems from './user/GovSchemes/GovScehems';
+import PatientProfile from './user/UserMain/PatientProfile/PatientProfile';
+import FindDonor from './user/Donor/FindDonor';
+import DonorList from './user/Donor/DonorList';
+import {Routes, Route} from 'react-router-dom'
+import { AuthContextProvider } from './FirebaseAuthContext/AuthContext';
+import Fundraiser from './user/Fundraiser/Fundraiser';
+import DonateOrgan from './DonorSide/DonateOrgan';
+import Analytics from './admin/Analytics';
+import Verification from './admin/Verification';
+import VideoCall from './user/Donor/VideoCall';
+
+
 
 function App() {
+  const [patient, setPatient] = useState(false);
+  // const [donor, setDonor] = useState(false);
+  // useEffect(()=>{}, [me]) 
+  useEffect(()=>{}, [patient])  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-full w-full scrollbar-hide">
+      <AuthContextProvider>
+      {patient && <Sidebar setPatient={setPatient} />}
+      <Routes>
+        <Route path='/login' element={<Login setPatient={setPatient}/>} />
+        <Route path='/signup' element={<Signup setPatient={setPatient}/>} />
+        <Route path='/community' element={<Community />} />
+        <Route path='/education' element={<Education />} />
+        <Route path='/blog' element={<BlogsPage />} />
+        <Route path='/edvideo' element={<VideosPage />} />
+        <Route path='/insurance' element={<Insurance />} />
+        <Route path='/schemes' element={<GovScehems />} />
+        <Route path='/patientprofile' element={<PatientProfile />} />
+        <Route path='/finddonor' element={<FindDonor />} />
+        <Route path='/donorlist' element={<DonorList />} />
+        <Route path='/fundraiser' element={<Fundraiser />} />
+        <Route path='/donateorgan' element={<DonateOrgan />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/verification' element={<Verification />} />
+        <Route path='/meeting' element={<VideoCall />} />
+      </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
+import { AnalysisProvider } from './user/Donor/hospitalContext';
+import { RoleProvider } from './login_signup/roleContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <RoleProvider>
+      <AnalysisProvider>
+        <App />
+      </AnalysisProvider>
+    </RoleProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
